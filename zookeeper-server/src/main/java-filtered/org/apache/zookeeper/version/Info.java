82c78a23829adfb46abdbff848c2de16483509c4
caomingjie <caomingjie>
2021-11-12 20:40:16 +0800

    添加 编译注释

--- zookeeper-server/src/main/java-filtered/org/apache/zookeeper/version/Info.java
+++ zookeeper-server/src/main/java-filtered/org/apache/zookeeper/version/Info.java
@@ -19,11 +19,21 @@
 package org.apache.zookeeper.version;
 
 public interface Info {
-    int MAJOR=${parsedVersion.majorVersion};
-    int MINOR=${parsedVersion.minorVersion};
-    int MICRO=${parsedVersion.incrementalVersion};
-    String QUALIFIER="${parsedVersion.qualifier}".isEmpty() ? null : "${parsedVersion.qualifier}";
-    int REVISION=-1; //@deprecated, please use REVISION_HASH
-    String REVISION_HASH="${mvngit.commit.id}";
-    String BUILD_DATE="${build.time}";
+//    int MAJOR=${parsedVersion.majorVersion};
+//    int MINOR=${parsedVersion.minorVersion};
+//    int MICRO=${parsedVersion.incrementalVersion};
+//    String QUALIFIER="${parsedVersion.qualifier}".isEmpty() ? null : "${parsedVersion.qualifier}";
+//    int REVISION=-1; //@deprecated, please use REVISION_HASH
+//    String REVISION_HASH="${mvngit.commit.id}";
+//    String BUILD_DATE="${build.time}";
+    /**
+     * 一下拷贝自 zk jar 包中的代码
+     */
+    public static final int MAJOR=3;
+    public static final int MINOR=4;
+    public static final int MICRO=9;
+    public static final String QUALIFIER=null;
+    int REVISION=-1;
+    public static final String REVISION_HASH=1757313+"";
+    public static final String BUILD_DATE="08/23/2016 08:54 GMT";
 }
